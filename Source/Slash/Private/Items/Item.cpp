49de69715ce8143c799fe0f5f7bd5fa7ec716438
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-04-29 12:41:43 -0300

    Define the macro in the Slash.h so it can be used by any other class as long as it includes Slash.h

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -3,8 +3,9 @@
 
 #include "Items/Item.h"
 #include "DrawDebugHelpers.h"
+#include "Slash/Slash.h"
 
-#define DRAW_SPHERE(Location) if (GetWorld()) DrawDebugSphere(GetWorld(), Location, 25.f, 12, FColor::Red, true);
+
 
 // Sets default values
 AItem::AItem()
