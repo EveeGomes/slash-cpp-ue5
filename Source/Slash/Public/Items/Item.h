e61311cde0d243bb4c4c0f7b27c7aae1996df891
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-05-02 10:51:13 -0300

    Add UPROPERTY specifiers to make possible to work and visualise variables; meta to allow private variables to be accessed without changing it to public or protected in cpp code

--- Source/Slash/Public/Items/Item.h
+++ Source/Slash/Public/Items/Item.h
@@ -19,17 +19,20 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Parameters")
+	float Amplitude = 0.25f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Parameters")
+	float TimeConstant = 5.f;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 private:
 	// Variable used to add DeltaTime to it every frame
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	float RunningTime;
 
-	UPROPERTY(EditAnywhere)
-	float Amplitude = 0.25f;
-	UPROPERTY(EditAnywhere)
-	float TimeConstant = 5.f;
+	
 
 };
