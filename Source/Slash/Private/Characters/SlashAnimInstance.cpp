0bce65c80dca8ea6cd9081dfab996175a47af92b
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-05-17 15:18:58 -0300

    Create AnimInstance CPP class and add the functions that correspond to the BP events used to set the animation

---
+++ Source/Slash/Private/Characters/SlashAnimInstance.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/SlashAnimInstance.h"
+
+void USlashAnimInstance::NativeInitializeAnimation()
+{
+   Super::NativeInitializeAnimation();
+}
+
+void USlashAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+   Super::NativeUpdateAnimation(DeltaTime);
+}
