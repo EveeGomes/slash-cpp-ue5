516fc353bab568e49e4273ddc3b073a7cf426d7b
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-05-29 09:05:13 -0300

    Set the item's state to equipped once the item is attached to the character's mesh component

--- Source/Slash/Private/Items/Weapons/Weapon.cpp
+++ Source/Slash/Private/Items/Weapons/Weapon.cpp
@@ -8,6 +8,10 @@ void AWeapon::Equip(USceneComponent* InParent, FName InSocketName)
 {
    FAttachmentTransformRules TransformRules(EAttachmentRule::SnapToTarget, true);
    ItemMesh->AttachToComponent(InParent, TransformRules, InSocketName);
+
+   // Set the item's state to Equipped since we've just attached its mesh to another component (the skeletal mesh of the character).
+   // After this is set, the tick function from the Item class will not execute the if statement that causes an item object to hover
+   ItemState = EItemState::EIS_Equipped; // This variable is in the protected section in Item.h!!!!!!
 }
 
 void AWeapon::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
