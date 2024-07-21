bd440d8c362460598ededb8ebdf7a224187f748b
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-07-21 20:07:09 -0300

    Add Enemy class. Create BP and imported mixamo character and animation. Used Blender to add a root bone for root motion animations.

---
+++ Source/Slash/Private/Enemy/Enemy.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enemy/Enemy.h"
+
+/** In order to use the skeletal mesh and capsule components */
+#include "Components/SkeletalMeshComponent.h"
+#include "Components/CapsuleComponent.h"
+
+
+// Sets default values
+AEnemy::AEnemy()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	/** 
+	* Setup the properties of the mesh component(which is inherented from the ACharacter class).
+	* The collision object type of this mesh needs to be set to WorldDynamic because the Weapon class is set to ignore Pawn type.
+	* Also, set the response to the visibility channel to be blocking it.
+	* Set the mesh to generate overlap events, otherwise it won't do anything.
+	* 
+	* Ignore the camera channel so it won't zoom in when colliding with the enemy mesh.
+	* Make the capsule ignore the camera as well (as it can block the camera too).
+	*/
+
+	GetMesh()->SetCollisionObjectType(ECollisionChannel::ECC_WorldDynamic);
+	GetMesh()->SetCollisionResponseToChannel(ECollisionChannel::ECC_Visibility, ECollisionResponse::ECR_Block);
+	GetMesh()->SetGenerateOverlapEvents(true);
+
+	// Ignore the camera
+	GetMesh()->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECollisionResponse::ECR_Ignore);
+	GetCapsuleComponent()->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECollisionResponse::ECR_Ignore);
+
+	
+}
+
+// Called when the game starts or when spawned
+void AEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
