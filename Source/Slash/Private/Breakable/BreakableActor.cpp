41081fb178af3cabed1a7f4002168da3f288860a
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-07-26 20:35:58 -0300

    Construct a geometry collection
    - BreakableActor implements a geometry collection so when broken it can
      perform some kind of functionality, such as giving coins or any other
      feature to the player.
    - Add the module GeometryCollectionEngine to build.cs so the compiler
      recongnizes it and remove the external error complain.

---
+++ Source/Slash/Private/Breakable/BreakableActor.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Breakable/BreakableActor.h"
+
+#include "GeometryCollection/GeometryCollectionComponent.h"
+
+// Sets default values
+ABreakableActor::ABreakableActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it. <-
+	PrimaryActorTick.bCanEverTick = false;
+
+	// Construct the UGeometryCollectionComponent
+	GeometryCollection = CreateDefaultSubobject<UGeometryCollectionComponent>(TEXT("GeometryCollection"));
+	// As it derives from USceneComponent, we can make it the root component
+	SetRootComponent(GeometryCollection);
+	// Set Generate Overlap Events here as it becomes the default setting
+	GeometryCollection->SetGenerateOverlapEvents(true);
+
+}
+
+// Called when the game starts or when spawned
+void ABreakableActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABreakableActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
