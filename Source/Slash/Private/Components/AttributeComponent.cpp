12f187a6917c844bd7a8138d8676f5a429bd0971
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-07-30 11:59:50 -0300

    Create an Actor Component to be added in other classes as needed.

---
+++ Source/Slash/Private/Components/AttributeComponent.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/AttributeComponent.h"
+
+// Sets default values for this component's properties
+UAttributeComponent::UAttributeComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+
+}
+
+// Called when the game starts
+void UAttributeComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+
+}
+
+
+void UAttributeComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+}
+
