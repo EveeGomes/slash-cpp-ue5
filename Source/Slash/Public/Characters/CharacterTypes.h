d9b471da32c34b1d4c6fdf1639dccb73d5f85c02
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-08-15 09:15:14 -0300

    Move NoState to be the first enum constant so in the BP it'll be recognized as the default in ABP_Paladin

--- Source/Slash/Public/Characters/CharacterTypes.h
+++ Source/Slash/Public/Characters/CharacterTypes.h
@@ -37,13 +37,13 @@ enum EDeathPose
 UENUM(BlueprintType)
 enum class EEnemyState : uint8
 {
+	EES_NoState UMETA(DisplayName = "NoState"),
+	
 	EES_Dead UMETA(DisplayName = "Dead"), // the most dorment state the enemy can be
 	EES_Idle UMETA(DisplayName = "Idle"),
 	EES_IdlePatrol UMETA(DisplayName = "Idle Patrol"),
 	EES_Patrolling UMETA(DisplayName = "Patrolling"),
 	EES_Chasing UMETA(DisplayName = "Chasing"),
 	EES_Attacking UMETA(DisplayName = "Attacking"),
-	EES_Engaged UMETA(DisplayName = "Engaged"),
-
-	EES_NoState UMETA(DisplayName = "NoState")
+	EES_Engaged UMETA(DisplayName = "Engaged")
 };
