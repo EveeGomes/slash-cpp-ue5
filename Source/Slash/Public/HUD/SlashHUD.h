bbe47c0fa88dd2fd9bfade421bbce607a9217a9b
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-08-22 09:11:49 -0300

    Hud C++ class to reparent BP_SlashHUD

---
+++ Source/Slash/Public/HUD/SlashHUD.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "SlashHUD.generated.h"
+
+/**
+ * 
+ */
+
+class USlashOverlay;
+
+UCLASS()
+class SLASH_API ASlashHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Slash")
+	TSubclassOf<USlashOverlay> SlashOverlayClass;
+};
