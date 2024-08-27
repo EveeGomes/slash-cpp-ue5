1861adf6b105389848bbb4403132dd25807c6d9d
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-08-27 18:10:56 -0300

    Move Drifing soul mechanic to c++

--- Source/Slash/Private/Items/Soul.cpp
+++ Source/Slash/Private/Items/Soul.cpp
@@ -3,6 +3,38 @@
 
 #include "Items/Soul.h"
 #include "Interfaces/PickupInterface.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+void ASoul::BeginPlay()
+{
+	Super::BeginPlay();
+
+	const FVector Start = GetActorLocation();
+	const FVector End = Start - FVector{ 0.f, 0.f, 2000.f };
+
+	/** TEnumAsByte is just an enum in the form of an eight bit integer, an unsigned int. */
+	TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypes;
+	ObjectTypes.Add(EObjectTypeQuery::ObjectTypeQuery1);
+
+	TArray<AActor*> ActorsToIgnore;
+	ActorsToIgnore.Add(GetOwner()); // so the owner of this soul is ignore by the trace
+
+	FHitResult HitResult;
+
+	UKismetSystemLibrary::LineTraceSingleForObjects(
+		this,
+		Start,
+		End,
+		ObjectTypes,
+		false,
+		ActorsToIgnore,
+		EDrawDebugTrace::ForDuration,
+		HitResult,
+		true
+	);
+
+	DesiredZ = HitResult.ImpactPoint.Z + 100.;
+}
 
 void ASoul::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
@@ -14,5 +46,19 @@ void ASoul::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* Ot
 		SpawnPickupSystem();
 		SpawnPickupSound();
 		Destroy();
+	}
+}
+
+void ASoul::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	const double ZActorLocation = GetActorLocation().Z;
+
+	if (ZActorLocation > DesiredZ)
+	{
+		AddActorWorldOffset(
+			FVector{ 0., 0., DeltaTime * DriftRate }
+		);
 	}
 }
--- Source/Slash/Public/Items/Soul.h
+++ Source/Slash/Public/Items/Soul.h
@@ -18,7 +18,15 @@ private:
 	UPROPERTY(EditAnywhere, Category = "Soul Properties")
 	int32 Souls;
 
+	UPROPERTY(EditAnywhere, Category = "Soul Properties")
+	double DesiredZ = 0.;
+
+	UPROPERTY(EditAnywhere, Category = "Soul Properties")
+	double DriftRate = -20.;
+
 protected:
+	virtual void BeginPlay() override;
+
 	virtual void OnSphereOverlap(
 		UPrimitiveComponent* OverlappedComponent,
 		AActor* OtherActor,
@@ -29,6 +37,8 @@ protected:
 	) override;
 
 public:
+	virtual void Tick(float DeltaTime) override;
+	/** Getters and Setters */
 	FORCEINLINE int32 GetSouls() const { return Souls; }
 	FORCEINLINE void SetSouls(int32 NumberOfSouls) { Souls = NumberOfSouls; }
 };
